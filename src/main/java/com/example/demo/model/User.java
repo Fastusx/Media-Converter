101bcd22718824e160d6d353f74a910ee006253d
Arthur dos Santos Lima <arthurdoslima@gmail.com>
2026-07-02 16:51:52 -0300

    refactor: adding collumn "role" to database "users"

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -19,13 +19,17 @@ public class User {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Column(nullable = false)
+    private String role;
+
     public User() {
     }
 
-    public User(String email, String username, String passwordHash) {
+    public User(String email, String username, String passwordHash, String role) {
         this.email = email;
         this.username = username;
         this.passwordHash = passwordHash;
+        this.role = role;
     }
 
     // Getters e Setters
@@ -55,5 +59,13 @@ public class User {
 
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
